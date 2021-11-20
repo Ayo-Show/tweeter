808294a2b9c0ab7fc46721b366fa390ef42aa03e
Ayo-Show <sowunmi30@gmail.com>
2021-11-20 13:26:31 -0500

    Update to variable declaration

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   // --- our code goes here ---
   $(document).ready(function () {
     $("textarea").keyup(function () {
-      let value = $(this).val();
-      let maxlength = $(this).data("maxlength");
+      const value = $(this).val();
+      const maxlength = $(this).data("maxlength");
   
       let total = maxlength - value.length;
       console.log(total);
